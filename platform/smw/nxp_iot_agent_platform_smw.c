d7a82581cefe4e2348ebf008df12cd87c28b3ade
Bojan Simoneta <bojan.simoneta@nxp.com>
2024-10-15 14:15:59 +0200

    Prepare the platform files including the platform init function for Se05x, TFM and SMW
    Signed-off-by: Bojan Simoneta <bojan.simoneta@nxp.com>

---
+++ platform/smw/nxp_iot_agent_platform_smw.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2024 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#include <nxp_iot_agent_platform.h>
+#include <nxp_iot_agent_log.h>
+#include <errno.h>
+#include <smw_osal.h>
+
+iot_agent_status_t iot_agent_platform_init(int argc, const char *argv[], iot_agent_platform_context_t* platform_context)
+{
+	int res;
+
+	res = smw_osal_lib_init();
+	if (res != SMW_STATUS_OK) {
+		IOT_AGENT_ERROR("SMW library initialization failed %d", res);
+		return IOT_AGENT_FAILURE;
+	}
+
+	return IOT_AGENT_SUCCESS;
+}
